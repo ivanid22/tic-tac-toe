5af3a0d6bfd3e59e294a8e387b59d22294ed0def
FrancoRosa <km115.franco@gmail.com>
2020-08-04 15:58:02 -0500

    Add verify function

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 const gameBoard = ( () => {
 
-  const board = ['X', 'O', 'X', 'X', 'O', 'X', 'X', 'O', 'X'];
+  const board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
   
   const getElement = (index) => board[index]
 
@@ -10,7 +10,8 @@ const gameBoard = ( () => {
 
   return {
     getElement,
-    setElement
+    setElement,
+    board
   }
 
 })()
@@ -19,7 +20,44 @@ const renderBoard = () => {
   gridElements = document.querySelectorAll('.grid-element');
   gridElements.forEach((element, index) => {
     element.innerText = gameBoard.getElement(index);
+    element.onclick = function() {
+      element.textContent = "#";
+      gameBoard.setElement('#', element.attributes[0].nodeValue);
+      console.log(gameBoard.getElement(element.attributes[0].nodeValue))
+      element.onclick = null;
+
+    } 
   });
+}
+
+const Player = (name1, symbol1) => {
+  const name = name1;
+  const symbol = symbol1;
+  const getName = () => name;
+  const getSymbol = () => symbol;
+  return {
+    getName,
+    getSymbol
+  }
+}
+
+const player1 = Player('P1', 'X')
+const player2 = Player('P2', 'O')
+
+const game = ( (player1, player2) => {
+  
+  
+})
+
+function verify(board){
+  
+  const winnerPositions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+  for (test of winnerPositions){
+    if (board[test[0]] === board[test[1]] && 
+        board[test[0]] === board[test[2]] && 
+        board[test[0]] != ' ') return true;
+  }
+  return false;
 }
 
 window.onload = renderBoard;
